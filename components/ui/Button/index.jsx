ca9ef7b8f0546298c0a1c427a8f810a05489324a
miillaadddddd <milad7212@gmail.com>
2022-01-25 23:10:14 +0330

    Feat : structrue for Button

---
+++ components/ui/Button/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './Button.module.scss'
+
+function Button() {
+  return <div>dddd</div>;
+}
+
+export default Button;
---
+++ pages/test/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '../../components/ui/Button';
+
+function index() {
+  return <>
+  <div className="h-screen flex justify-center items-center bg-red-200">
+
+      <Button/>
+  </div>
+  
+  
+  </>;
+}
+
+export default index;
