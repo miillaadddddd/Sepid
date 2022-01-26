9acf00665b1100cbe009995da901c0e269cf28ea
miillaadddddd <milad7212@gmail.com>
2022-01-26 22:59:44 +0330

    improve structure

---
+++ components/ui/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Button.module.css'
+
+function Button() {
+  return <>
+  <button className={s.root}> fuck</button>
+  </>;
+}
+
+export default Button;
--- components/ui/Button/index.jsx
+++ components/ui/Button/index.jsx
@@ -1,8 +1,1 @@
-import React from 'react';
-import s from './Button.module.scss'
-
-function Button() {
-  return <div>dddd</div>;
-}
-
-export default Button;
+export { default } from './Button'
